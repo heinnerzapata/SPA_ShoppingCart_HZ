cc190fa75316c439fad70d250e0bb41c0525f45d
Heinner Zapata Escarraga <heinner.zapata@SINCO.NET>
2017-02-09 11:31:30 -0500

    Folder structure

---
+++ controllers/app.js
@@ -0,0 +1,24 @@
+var mainApp = angular.module('mainApp',['ngRoute']);
+
+mainApp.config(function($routeProvider , $locationProvider){
+
+   $routeProvider
+     .when('/home',{
+       templateUrl:'views/home.html',
+       controller:'homeController'
+     })
+    .otherwise({
+       redirectTo:'/home'
+     });
+
+     // use the HTML5 History API
+     //$locationProvider.html5Mode(true);
+
+ });
+
+
+ mainApp.controller('homeController',function homeController($scope,$http) {
+
+
+
+ });

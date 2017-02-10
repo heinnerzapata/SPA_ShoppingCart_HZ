48e8ce69dd615b3728d57fe60fab1a467acbcdf0
Heinner Zapata Escarraga <heinner.zapata@SINCO.NET>
2017-02-10 15:47:32 -0500

    Header icons and animations bugs

--- js/template/template.js
+++ js/template/template.js
@@ -2,6 +2,7 @@ $window = $(window);
 $window.ready(function(){
 
   function menuAction(){
+
     if($("#movileMenu").hasClass("menuOculto"))
     {
         $("#movileMenu").removeClass("menuOculto");
@@ -14,17 +15,59 @@ $window.ready(function(){
     }
   }
 
+  function showTestAllImagesStates(NoIter){
+
+    var i=1;
+    for(i=1;i<=NoIter;i++)
+    {
+      if($("#imgFoodConatiner" + String(i)).hasClass("imgFoodConatiner" + String(i) + "_P1"))
+      {
+          $("#imgFoodConatiner" + String(i)).removeClass("imgFoodConatiner" + String(i) +"_P1");
+          $("#imgFoodConatiner" + String(i)).addClass("imgFoodConatiner" +  String(i) + "_P2");
+      }
+    }
+
+  }
+
   function initAnimations()
   {
-    if($("#imgFoodConatiner1").hasClass("imgFoodConatiner1_P1"))
-    {
-        $("#imgFoodConatiner1").removeClass("imgFoodConatiner1_P1");
-        $("#imgFoodConatiner1").addClass("imgFoodConatiner1_P2");
-    }
+    showTestAllImagesStates(2);
   }
 
   $("body").on("click","#iconMenuContainer",function(){
      menuAction();
+  });
+
+
+  $("body").on("click","#imgScrollIcon",function(){
+    var scroll = $(window).scrollTop();
+
+    if($("#imgScrollIcon").hasClass("ImgScrollUp"))
+    {
+        $(window).scrollTop(0);
+
+        //$.scrollTo($('#pageLogoTitleContainerChild'), 1000);
+
+    }
+
+  });
+
+  $(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+
+    if(scroll > 0 && $("#imgScrollIcon").hasClass("ImgScrollDown"))
+    {
+      $("#imgScrollIcon").removeClass("ImgScrollDown");
+      $("#imgScrollIcon").addClass("ImgScrollUp");
+    }
+
+    if(scroll == 0 && $("#imgScrollIcon").hasClass("ImgScrollUp"))
+    {
+      $("#imgScrollIcon").removeClass("ImgScrollUp");
+      $("#imgScrollIcon").addClass("ImgScrollDown");
+    }
+
+
   });
 
   setTimeout(function(){ initAnimations(); }, 250);

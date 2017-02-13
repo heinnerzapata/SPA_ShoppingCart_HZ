c1bc22ed336b808883789884f208167a156052da
HeinnerYesidZapata <mrhz@localhost.localdomain>
2017-02-12 23:34:13 -0500

    Lateral filter

--- controllers/app.js
+++ controllers/app.js
@@ -1,4 +1,5 @@
-var mainApp = angular.module('mainApp',['ngRoute']);
+var mainApp = angular.module('mainApp',['ngRoute','angularUtils.directives.dirPagination']);
+
 
 mainApp.config(function($routeProvider , $locationProvider){
 
@@ -34,22 +35,177 @@ mainApp.config(function($routeProvider , $locationProvider){
     $scope.modalProduct =  []; 
     $scope.productSource = "local";
     $scope.externalPath = "";
+    $scope.filterName = "";
+    $scope.filterBest = "all";
+    $scope.filterPriceRange = "all";
+    $scope.filterOrderBy = 'name';
+    $scope.carShopping = [];
+
+    /*Filters*/
 
 
-    $scope.getLoalProducts =  function(){
-      
+    $scope.filterAll=function(value) {
 
-      $http.get("/api/products")
-        .then(function(response) {
-        
-          $scope.products = JSON.parse(response.data).products;  
-          $scope.categories = JSON.parse(response.data).categories;              
-      
-        });
+      var res = false;
+
+      res = $scope.filterCheckBestSeller(value) && $scope.filterCheckPriceRange(value);
+
+      return res;
+
+    }
+    
+
+
+    $scope.filterCheckBestSeller=function(value) {
+
+      if($scope.filterBest == "all")
+      {
+        return true;
+      }
+
+      if ((value.best_seller===true) && $scope.filterBest == "best") {
+        return true;
+      } else {
+        return false;
+      }
+
 
     }
 
-    $scope.searchProducts =  function(){
+
+    $scope.filterCheckPriceRange=function(value) {
+      
+      if($scope.filterPriceRange == "all")
+      {
+        return true;
+      }
+
+      if (  parseFloat(value.price) >= 30 && $scope.filterPriceRange == "30") {
+        return true;
+      } 
+
+      if (  parseFloat(value.price) <= 10 && $scope.filterPriceRange == "10") {
+        return true;
+      }
+      else
+      {
+        return false; 
+      }      
+
+    }
+
+
+    /*Filters*/
+
+
+    $scope.addCarProduct = function(){
+      
+      var $idCurrrentProduct = $scope.modalProduct.id;
+      var isCurrentInCarShopping=false;
+
+      if($scope.carShopping.length > 0 ){
+        
+        alert($scope.carShopping[0].units);
+        alert($scope.carShopping.length);
+
+      }
+
+      var i = 0;
+      var posId = 0;
+      var $prevCant = 0;
+      for(i = 0;i<$scope.carShopping.length;i++)
+      {
+        if($scope.carShopping[i].id == $idCurrrentProduct)
+        {
+          isCurrentInCarShopping = true;
+          $prevCant = parseInt($scope.carShopping[i].units);
+          //alert($scope.carShopping);
+          break;
+        }
+      }
+
+      if(!isCurrentInCarShopping)
+      {
+        $scope.carShopping.push($scope.modalProduct);
+      }
+      else
+      {
+        $scope.carShopping[i].units = parseInt($prevCant) + parseInt($scope.modalProduct.units);
+      }
+
+   
+      $("#close-modal1").click();
+
+    }
+
+    $scope.showMessage = function(){
+            var $msg = document.getElementById("msg");
+            if($msg.classList.contains("hidden"))
+            {
+              $msg.classList.remove("hidden");     
+              $msg.classList.add("shown");
+            }
+    }
+
+
+    $scope.HideMessage = function(){
+            var $msg = document.getElementById("msg");
+            if($msg.classList.contains("shown"))
+            {
+              $msg.classList.remove("shown");     
+              $msg.classList.add("hidden");
+            } 
+    }
+
+    $scope.getLoalProducts =  function(){
+     
+        $scope.products =  []; 
+
+        $http.get("/api/products").then(function mySucces(response) {
+            
+            $scope.products = JSON.parse(response.data).products;  
+            $scope.categories = JSON.parse(response.data).categories;
+
+            $scope.HideMessage();
+
+
+        }, function myError(response) {
+            
+            
+            $scope.showMessage();
+            //d.classList.add("otherclass");
+
+        
+        });
+        
+
+    }
+
+
+    $scope.getExternProducts =  function(path){
+        
+        $scope.products =  []; 
+
+        $http.get(path).then(function mySucces(response) {
+            
+            $scope.products = JSON.parse(response.data).products;  
+            $scope.categories = JSON.parse(response.data).categories;              
+
+            $scope.HideMessage();
+
+        }, function myError(response) {
+            
+              $scope.showMessage();
+        
+        });
+
+
+    }
+
+
+    
+
+    $scope.searchProducts =  function(path){
       
       if($scope.productSource == "local")
       {
@@ -59,11 +215,19 @@ mainApp.config(function($routeProvider , $locationProvider){
       }
 
 
+      if($scope.productSource == "extern")
+      {
+
+        $scope.getExternProducts(path);
+
+      }
+
+
     }
 
     $scope.init =  function(){
 
-      $scope.searchProducts();
+      $scope.searchProducts("");
     }
 
     $scope.productDetail = function(product){
